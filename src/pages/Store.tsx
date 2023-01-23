2c6a318253518d9a3f3ca9bbb561fb99abcc9df4
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-01-23 20:13:21 +0300

    style the store page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,16 @@
-import React from "react";
+import StoreItem from "../components/StoreItem";
+import Products from "../data/store-data.json";
 
 export default function Store() {
-  return <div>Store</div>;
+  return (
+    <div className="w-max-7xl pt-3 px-4 lg:px-8">
+      <h1 className="text-center font-bold text-3xl mb-4">Store</h1>
+
+      <div className="grid grd-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 ">
+        {Products.map((product) => (
+          <StoreItem {...product} key={product.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
