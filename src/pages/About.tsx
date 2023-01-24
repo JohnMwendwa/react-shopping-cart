c0026a851ff4443e1968cd8e936429571e204b6f
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-01-24 14:30:20 +0300

    style the about page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="px-4 lg:px-6 max-w-7xl text-center ">
+      <h1 className="font-medium text-2xl my-4 text">About this Cart</h1>
+      <p>
+        This shopping cart was created using | React | TypeScript | Tailwind |
+      </p>
+    </div>
+  );
 }
