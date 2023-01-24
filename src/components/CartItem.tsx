c355cf7f7ad14045183f13ffe5b70775bb1cacb3
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-01-24 10:47:05 +0300

    add Cart Item functionality

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,5 +1,40 @@
-import React from "react";
+import { useShoppingCartContext } from "../context/shoppingCartContext";
+import products from "../data/store-data.json";
+import { formatCurrency } from "../utils/formatCurrency";
 
-export default function CartItem() {
-  return <div>CartItem</div>;
+interface CartItemProps {
+  id: number;
+  quantity: number;
+}
+
+export default function CartItem({ id, quantity }: CartItemProps) {
+  const { removeFromCart } = useShoppingCartContext();
+  const item = products.find((product) => product.id === id);
+  if (item == null) return null;
+
+  return (
+    <div className="flex justify-between items-center w-full border-2">
+      <div className="flex">
+        <img
+          src={item.imgUrl}
+          alt={item.name}
+          width="125px"
+          height="75px"
+          className="rounded-md object-cover"
+        />
+
+        <div>
+          <div>
+            <span>{item.name}</span>
+            {quantity > 1 && <span> x {quantity}</span>}
+          </div>
+          <div>{formatCurrency(item.price)}</div>
+        </div>
+      </div>
+      <div>
+        {formatCurrency(item.price * quantity)}
+        <button onClick={() => removeFromCart(id)}>&#10761;</button>
+      </div>
+    </div>
+  );
 }
