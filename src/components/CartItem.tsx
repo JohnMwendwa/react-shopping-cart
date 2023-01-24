9f9c5eea4d030dabfd0d72b741c5377f2cb6b7eb
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-01-24 12:05:22 +0300

    add more styles to the CartItem component

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -13,24 +13,24 @@ export default function CartItem({ id, quantity }: CartItemProps) {
   if (item == null) return null;
 
   return (
-    <div className="flex justify-between items-center w-full border-2 px-4 py-3">
+    <div className="flex justify-between items-center w-full  px-1 py-3 ">
       <div className="flex items-center">
         <img
           src={item.imgUrl}
           alt={item.name}
-          className="rounded-md object-cover w-[125px] h-[75px]"
+          className="rounded object-cover w-[125px] h-[75px]"
         />
 
         <div className="ml-3">
           <div>
             <span className="font-medium text-xl">{item.name}</span>
-            {quantity > 1 && <span> x {quantity}</span>}
+            {quantity > 1 && <span className="text-xs"> x {quantity}</span>}
           </div>
-          <div>{formatCurrency(item.price)}</div>
+          <div className="text-sm">{formatCurrency(item.price)}</div>
         </div>
       </div>
-      <div>
-        {formatCurrency(item.price * quantity)}
+      <div className="flex flex-col justify-center items-end ml-2 md:flex-row md:items-center">
+        <span> {formatCurrency(item.price * quantity)}</span>
         <button
           onClick={() => removeFromCart(id)}
           className="border text-red-500 font-extrabold px-2 py-1 rounded-md ml-2"
