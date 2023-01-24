fe64fb51466f75b89218408f95181ac8898d4a5b
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-01-24 09:56:48 +0300

    map through the Cart items and render individual cart item

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,11 +1,17 @@
 import { useShoppingCartContext } from "../context/shoppingCartContext";
+import CartItem from "./CartItem";
 
 export default function ShoppingCart() {
   const { cartItems, closeCart } = useShoppingCartContext();
   return (
     <div>
       <button onClick={closeCart}> &#10761;</button>
-      <h1> ShoppingCart</h1>
+      <h1> Shopping Cart</h1>
+      <div>
+        {cartItems.map((item) => (
+          <CartItem key={item.id} {...item} />
+        ))}
+      </div>
     </div>
   );
 }
