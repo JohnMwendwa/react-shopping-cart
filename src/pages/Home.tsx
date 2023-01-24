1fe8135554d4e41e24eaab34d28034150a1beef5
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-01-24 14:21:02 +0300

    style the Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="px-4 lg:px-6 py-3">
+      <h1 className="text-center font-medium text-2xl my-4 text">
+        Welcome to our Store
+      </h1>
+    </div>
+  );
 }
