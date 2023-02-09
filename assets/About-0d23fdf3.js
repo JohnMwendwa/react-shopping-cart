7974ffaaef19fd6e4f2d696ba3fe4fa44fdc0eb5
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-02-09 17:32:01 +0300

    deploy

---
+++ assets/About-0d23fdf3.js
@@ -0,0 +1,1 @@
+import{j as e,a as t}from"./index-db0cc376.js";function s(){return e("div",{className:"px-4 lg:px-6 max-w-7xl text-center ",children:[t("h1",{className:"font-medium text-2xl my-4 text",children:"About this Cart"}),t("p",{children:"This shopping cart was created using | React | TypeScript | Tailwind |"})]})}export{s as default};
